885f5f821cc2a8036c23a1cabf2f907b03a5ee52
小川佳納 <ogwk@ogawakeiosamenoMacBook-Pro.local>
2021-06-17 00:09:28 +0900

    addsquare

--- js/script.js
+++ js/script.js
@@ -1,32 +1,48 @@
 $(function(){
   var Timer;  
   var count;
+  var runnning = 0;
   $(".startbtn").click(function(){
-    $(".screen").children().remove();
-    count = 0;
-    var result = $(".screen").find(".circle");
-    if(result.length === 0){
+    try {
+      if(runnning === 0){
+        runnning = 1;
+        $(".screen").children().remove();
+        count = 0;
+        Timer = setInterval(function () {
 
-      count = 0;
-      Timer = setInterval(function () {
+          // addCircle(count);        
+          addSquare(count);        
+        　count++;
 
-        addCircle(count);        
-      　count++;
-        // 経過時間 >= 待機時間の場合、待機終了。
-        if (count >= 19) {
-            // タイマー停止
-            // clearInterval(Timer);
-            //クリア
-            $(".screen").children().remove();
-            count = 0;
-        };        
-
-        }, 500);
-    };
+          if (count >= 19) {
+              //クリア
+              $(".screen").children().remove();
+              count = 0;
+          };        
+        }, 50);
+      };
+    } catch (error) {
+      alert(error);
+      // expected output: ReferenceError: nonExistentFunction is not defined
+      // Note - error messages will vary depending on browser
+    }
   });
+
   $(".endbtn").click(function(){
-    //タイマー停止
-    clearInterval(Timer);
+    try {
+      //タイマー停止
+      if(runnning === 1){
+        clearInterval(Timer);
+        runnning = 0;
+      }else{
+        // break;
+      };    
+    } catch (error) {
+      alert(error);
+      // expected output: ReferenceError: nonExistentFunction is not defined
+      // Note - error messages will vary depending on browser
+    }    
+    
   });
 });
 
@@ -35,27 +51,30 @@ $(function(){
     if(count%2 === 0){
       var nowid = "circle" + count;    
       $(".screen").append('<div id="' + nowid + '" class="circle">');
-      // setSize(size,nowid,count);
-      // var obj = $('#' + nowid);    
-      // obj.width(size);
-      // obj.height(size);
-      // obj.css('z-index',18 - count);    
     }else{
       var nowid = "circle" + count + "-out";        
       $(".screen").append('<div id="' + nowid + '" class="circle-out">');
-      // setSize(size,nowid,count+5);    
-      // var obj = $('#' + nowid);   
-      // obj.width(size);
-      // obj.height(size);
-      // obj.css('z-index',18 - (count+1));  
-      // alert(obj.width());
     }
-    setSize(size,nowid,count);        
+    setSize(size,nsizeowid,count);        
+    setSize(size,size,nowid,count);        
   }
 
-  function setSize(size,nowid,count){
+  function addSquare(count){
+    var widthsize = count*50 + 100;
+    var heightsize = count*50 + 30;
+    if(count%2 === 0){
+      var nowid = "square" + count;    
+      $(".screen").append('<div id="' + nowid + '" class="square">');
+    }else{
+      var nowid = "square" + count + "-out";        
+      $(".screen").append('<div id="' + nowid + '" class="square-out">');
+    }
+    setSize(widthsize, heightsize,nowid,count);        
+  }
+
+  function setSize(widthsize,heightsize,nowid,count){
     var obj = $('#' + nowid);   
-    obj.width(size);
-    obj.height(size);
+    obj.width(widthsize);
+    obj.height(heightsize);
     obj.css('z-index',500 - (count));  
   }
